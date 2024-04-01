44579e14662ef3b512fb3c77f034b4ff63a3fa71
medcne <1692841880@qq.com>
2024-04-01 20:59:34 +0800

    [新增]新增切面类LogAspect，打印请求参数和结果，以及日志流水号。

--- train-member/src/main/java/com/leoliu/train/MemberApplication.java
+++ train-member/src/main/java/com/leoliu/train/MemberApplication.java
@@ -1,15 +1,11 @@
-package com.leoliu.train.member;
+package com.leoliu.train;
 
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@Slf4j
 @SpringBootApplication
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
-        log.info("启动成功");
-        log.error("错误");
     }
 }
