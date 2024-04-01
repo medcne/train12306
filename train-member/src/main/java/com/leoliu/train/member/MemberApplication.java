e19b17ddc70b590485d6e7a98cd076088d1ee180
medcne <1692841880@qq.com>
2024-04-01 17:24:34 +0800

    [新增]新增会员模块

--- train-member/src/main/java/com/leoliu/train/member/MemberApplication.java
+++ train-member/src/main/java/com/leoliu/train/member/MemberApplication.java
@@ -1,12 +1,15 @@
 package com.leoliu.train.member;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class,args);
+        log.info("启动成功");
+        log.error("错误");
     }
 }
