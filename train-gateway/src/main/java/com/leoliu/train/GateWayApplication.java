887f248c597563cc7d28a6acdea2e45ae1538d42
medcne <1692841880@qq.com>
2024-04-02 10:06:06 +0800

    [新增]新增网关模块与路由转发，配置网关日志开启-Dreactor.netty.http.server.accessLogEnabled=true

--- train-gateway/src/main/java/com/leoliu/train/GateWayApplication.java
+++ train-gateway/src/main/java/com/leoliu/train/GateWayApplication.java
@@ -1,14 +1,13 @@
 package com.leoliu.train;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MemberApplication {
+public class GateWayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class,args);
+        SpringApplication.run(GateWayApplication.class,args);
     }
 }
--- train-member/src/main/java/com/leoliu/train/controller/MemberController.java
+++ train-member/src/main/java/com/leoliu/train/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.leoliu.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/member")
 public class MemberController {
-    @GetMapping("/member")
+    @GetMapping("/test")
     public String hello(){
         return "hello member!";
     }
