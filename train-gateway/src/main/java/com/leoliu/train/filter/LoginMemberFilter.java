29ada7fd99ed5093b71e4ec08dfcdf50b50185b7
medcne <1692841880@qq.com>
2024-04-03 22:57:29 +0800

    [新增]新增前端axios拦截器，后端gateway统一拦截器filter

---
+++ train-gateway/src/main/java/com/leoliu/train/filter/LoginMemberFilter.java
@@ -0,0 +1,59 @@
+package com.leoliu.train.filter;
+
+import com.leoliu.train.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoginMemberFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //1.拿到请求的路径
+        String path = exchange.getRequest().getURI().getPath();
+        //2.排除不需要被访问的路径
+        if (path.contains("/admin")
+                || path.contains("/hello")
+                || path.contains("/member/login")
+                || path.contains("/member/send-code")
+        ) {
+            log.info("不需要登录验证：{}", path);
+            return chain.filter(exchange);
+        }else {
+            log.info("需要登录验证：{}", path);
+        }
+        //3.获取header的token参数
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        log.info("会员登录验证开始，token：{}", token);
+        if (token == null || token.isEmpty()) {
+            log.warn("token为空，请求被拦截" );
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        //4.校验token是否合法,有效期是否过期等
+        boolean validate = JwtUtil.validate(token);
+        if (validate){
+            log.info("token有效，放行该请求");
+            return chain.filter(exchange);
+        }
+        else {
+            log.warn("token无效，请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
--- web/src/views/TrainMain.vue
+++ web/src/views/TrainMain.vue
@@ -10,7 +10,7 @@
       <a-layout style="padding: 20px; background: #fff">
         <TrainSiderBar></TrainSiderBar>
         <a-layout-content :style="{ padding: '0 24px', minHeight: '500px' }">
-          Content
+          所有人数：{{count}}
         </a-layout-content>
       </a-layout>
     </a-layout-content>
@@ -23,19 +23,32 @@
 import { defineComponent, ref } from 'vue';
 import TrainHeader from "@/components/TrainHeader.vue";
 import TrainSiderBar from "@/components/TrainSiderBar.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
 export default defineComponent({
   components: {
     TrainSiderBar,
     TrainHeader,
-
   },
   setup() {
+    const count = ref(0)
+
+    axios.get("http://localhost:8000/member/count").then((response) => {
+      let data = response.data;
+      if (data.success) {
+        count.value = data.content;
+      } else {
+        notification.error({description: data.message});
+      }
+    });
     return {
       selectedKeys1: ref(['2']),
       selectedKeys2: ref(['1']),
       openKeys: ref(['sub1']),
+      count,
     };
-  },
+  }
+
 });
 </script>
 <style>
