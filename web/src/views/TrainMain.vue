f42557ae428d8b32bab3396238a322de2f42fa2f
medcne <1692841880@qq.com>
2024-04-03 11:24:07 +0800

    [新增]组件开发，增加SiderBar组件

--- web/src/views/TrainMain.vue
+++ web/src/views/TrainMain.vue
@@ -8,51 +8,7 @@
         <a-breadcrumb-item>App</a-breadcrumb-item>
       </a-breadcrumb>
       <a-layout style="padding: 20px; background: #fff">
-        <a-layout-sider width="240" style="background: #fff">
-          <a-menu
-              v-model:selectedKeys="selectedKeys2"
-              v-model:openKeys="openKeys"
-              mode="inline"
-              style="height: 100%"
-          >
-            <a-sub-menu key="sub1">
-              <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
-                </span>
-              </template>
-              <a-menu-item key="1">option1</a-menu-item>
-              <a-menu-item key="2">option2</a-menu-item>
-              <a-menu-item key="3">option3</a-menu-item>
-              <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-              </template>
-              <a-menu-item key="5">option5</a-menu-item>
-              <a-menu-item key="6">option6</a-menu-item>
-              <a-menu-item key="7">option7</a-menu-item>
-              <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-              </template>
-              <a-menu-item key="9">option9</a-menu-item>
-              <a-menu-item key="10">option10</a-menu-item>
-              <a-menu-item key="11">option11</a-menu-item>
-              <a-menu-item key="12">option12</a-menu-item>
-            </a-sub-menu>
-          </a-menu>
-        </a-layout-sider>
+        <TrainSiderBar></TrainSiderBar>
         <a-layout-content :style="{ padding: '0 24px', minHeight: '500px' }">
           Content
         </a-layout-content>
@@ -64,15 +20,14 @@
   </a-layout>
 </template>
 <script>
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref } from 'vue';
 import TrainHeader from "@/components/TrainHeader.vue";
+import TrainSiderBar from "@/components/TrainSiderBar.vue";
 export default defineComponent({
   components: {
+    TrainSiderBar,
     TrainHeader,
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
+
   },
   setup() {
     return {
