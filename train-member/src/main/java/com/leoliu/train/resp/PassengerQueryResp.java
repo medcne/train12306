16e0da5d9c7757068b4f4404a692b61630a1f5f5
medcne <1692841880@qq.com>
2024-04-04 17:10:28 +0800

    [新增]7-10 集成PageHelper实现后端分页（二）,格式化日期字段

--- train-member/src/main/java/com/leoliu/train/resp/PassengerQueryResp.java
+++ train-member/src/main/java/com/leoliu/train/resp/PassengerQueryResp.java
@@ -18,7 +18,7 @@ public class PassengerQueryResp {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+")
     private Date updateTime;
 
     public Long getId() {
