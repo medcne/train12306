2824a6ac1e8187e78b603cfdc7755a5aa6cc2bab
medcne <1692841880@qq.com>
2024-04-07 23:45:07 +0800

    [新增] 9.16 实现按车次生成车座功能，增加前端功能，完成 [新增] 9.16 实现按车次生成车座功能，增加后端接口，测试成功

--- train-business/src/main/java/com/leoliu/train/controller/admin/TrainAdminController.java
+++ train-business/src/main/java/com/leoliu/train/controller/admin/TrainAdminController.java
@@ -1,13 +1,12 @@
 package com.leoliu.train.controller.admin;
 
-
-import com.leoliu.train.context.LoginMemberContext;
 import com.leoliu.train.req.TrainQueryReq;
 import com.leoliu.train.req.TrainSaveReq;
+import com.leoliu.train.resp.TrainQueryResp;
+import com.leoliu.train.service.TrainSeatService;
+import com.leoliu.train.service.TrainService;
 import com.leoliu.train.resp.CommonResp;
 import com.leoliu.train.resp.PageResp;
-import com.leoliu.train.resp.TrainQueryResp;
-import com.leoliu.train.service.TrainService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
@@ -15,32 +14,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/business/admin/train")
+@RequestMapping("business/admin/train")
 public class TrainAdminController {
+
     @Resource
     private TrainService trainService;
 
+    @Resource
+    private TrainSeatService trainSeatService;
+
     @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody TrainSaveReq req){
+    public CommonResp<Object> save(@Valid @RequestBody TrainSaveReq req) {
         trainService.save(req);
         return new CommonResp<>();
     }
 
     @GetMapping("/query-list")
-    public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req){
-        PageResp<TrainQueryResp> pageResp = trainService.queryList(req);
-        return new CommonResp<>(pageResp);
+    public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req) {
+        PageResp<TrainQueryResp> list = trainService.queryList(req);
+        return new CommonResp<>(list);
     }
 
-    @DeleteMapping("delete/{id}")
-    public CommonResp<Object> delete(@PathVariable Long id){
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
     }
 
     @GetMapping("/query-all")
-    public CommonResp<List<TrainQueryResp>> queryAll(){
-        List<TrainQueryResp> queryRespList = trainService.queryAll();
-        return new CommonResp<>(queryRespList);
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
+
+    @GetMapping("/gen-seat/{trainCode}")
+    public CommonResp<Object> genSeat(@PathVariable String trainCode) {
+        trainSeatService.genTrainSeat(trainCode);
+        return new CommonResp<>();
+    }
+
+
+
 }
--- train-business/src/main/java/com/leoliu/train/service/TrainSeatService.java
+++ train-business/src/main/java/com/leoliu/train/service/TrainSeatService.java
@@ -3,10 +3,13 @@ package com.leoliu.train.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.leoliu.train.domain.TrainCarriage;
 import com.leoliu.train.domain.TrainSeat;
 import com.leoliu.train.domain.TrainSeatExample;
+import com.leoliu.train.enums.SeatColEnum;
 import com.leoliu.train.mapper.TrainSeatMapper;
 import com.leoliu.train.req.TrainSeatQueryReq;
 import com.leoliu.train.req.TrainSeatSaveReq;
@@ -16,6 +19,7 @@ import com.leoliu.train.util.SnowUtil;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,6 +28,8 @@ import java.util.List;
 public class TrainSeatService {
     @Resource
     private TrainSeatMapper trainSeatMapper;
+    @Resource
+    private TrainCarriageService trainCarriageService;
 
     public void save(TrainSeatSaveReq req) {
         DateTime now = DateTime.now();
@@ -63,7 +69,50 @@ public class TrainSeatService {
         pageResp.setList(list);
         return pageResp;
     }
+@Transactional
+public void genTrainSeat(String trainCode) {
+    DateTime now = DateTime.now();
+    // 清空当前车次下的所有的座位记录
+    TrainSeatExample trainSeatExample = new TrainSeatExample();
+    TrainSeatExample.Criteria criteria = trainSeatExample.createCriteria();
+    criteria.andTrainCodeEqualTo(trainCode);
+    trainSeatMapper.deleteByExample(trainSeatExample);
 
+    // 查找当前车次下的所有的车厢
+    List<TrainCarriage> carriageList = trainCarriageService.selectByTrainCode(trainCode);
+    log.info("当前车次下的车厢数：{}", carriageList.size());
+
+    // 循环生成每个车厢的座位
+    for (TrainCarriage trainCarriage : carriageList) {
+        // 拿到车厢数据：行数、座位类型(得到列数)
+        Integer rowCount = trainCarriage.getRowCount();
+        String seatType = trainCarriage.getSeatType();
+        int seatIndex = 1;
+
+        // 根据车厢的座位类型，筛选出所有的列，比如车箱类型是一等座，则筛选出columnList={ACDF}
+        List<SeatColEnum> colEnumList = SeatColEnum.getColsByType(seatType);
+        log.info("根据车厢的座位类型，筛选出所有的列：{}", colEnumList);
+
+        // 循环行数
+        for (int row = 1; row <= rowCount; row++) {
+            // 循环列数
+            for (SeatColEnum seatColEnum : colEnumList) {
+                // 构造座位数据并保存数据库
+                TrainSeat trainSeat = new TrainSeat();
+                trainSeat.setId(SnowUtil.getSnowflakeNextId());
+                trainSeat.setTrainCode(trainCode);
+                trainSeat.setCarriageIndex(trainCarriage.getIndex());
+                trainSeat.setRow(StrUtil.fillBefore(String.valueOf(row), '0', 2));
+                trainSeat.setCol(seatColEnum.getCode());
+                trainSeat.setSeatType(seatType);
+                trainSeat.setCarriageSeatIndex(seatIndex++);
+                trainSeat.setCreateTime(now);
+                trainSeat.setUpdateTime(now);
+                trainSeatMapper.insert(trainSeat);
+            }
+        }
+    }
+}
 
     public void delete(Long id) {
         trainSeatMapper.deleteByPrimaryKey(id);
